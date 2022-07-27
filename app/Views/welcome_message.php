da9be24d1a4fa65f592fc896fa9e614b25584ee5
BACKWARDS <nabilwafi88@gmail.com>
2022-07-27 20:04:33 +0700

    mengubah halaman depan

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -221,7 +221,7 @@
 
         <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
-        <h2>The small framework with powerful features</h2>
+        <h2>Nabil Wafi</h2>
 
     </div>
 
